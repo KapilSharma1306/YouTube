0d9ca56faa92f3cfcca38648baaadacf6d90a3b6
KapilSharma1306 <kapilSharmacoding1306202@gmail.com>
2024-11-12 21:51:04 +0530

    files saving

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='h-[100vh] w-[100vw] bg-black text-white font-sans'>
+      hello
+    </div>
+  )
+}
+
+export default App
